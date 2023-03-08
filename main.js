3983c8e2b155570cb74e4b18c2f7194cb91973de
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-08 07:40:28 -0500

    feat: :rocket: Method charCodeAt
    Los puntos de código Unicode van de 0 a 1114111 ( 0x10FFFF). Los primeros 128 puntos de  código Unicode son una coincidencia directa de la codificación de caracteres ASCII.

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-//                         charAt
-// Los caracteres de una cadena se indexan de izquierda a derecha. 
-// El índice del primer caracter es 0, y el índice del último 
-// caracter seria -1. Si el indice que se proporciona está fuera del rango, 
-// JavaScript devuelve una cadena vacía.
+//                                      charCodeAt
+// Los puntos de código Unicode van de 0 a 1114111 ( 0x10FFFF). Los primeros 128 puntos de 
+// código Unicode son una coincidencia directa de la codificación de caracteres ASCII.
+// charCodeAt() siempre devolverá un valor menor que 65536. Esto se debe a que los puntos 
+// de código más altos están representados por un par de pseudocaracteres "sustitutos" 
+// (de menor valor) que se utilizan para comprender el carácter real.
+// Debido a esto, para examinar (o reproducir) el carácter completo para valores de caracteres
+// individuales de 65536o mayores, para dichos caracteres, es necesario recuperar no solo 
+// charCodeAt(i), sino también charCodeAt(i+1)(como si se manipulara una cadena con dos letras),
+// o para usar codePointAt(i)en su lugar. Ver ejemplos 2 y 3 (abajo).
 
-const nombre = "Gadir"
 
-console.log(nombre.charAt(0))
-console.log(nombre.charAt(nombre.length - 1))
+let frase = "SHINZOU WO SASAGEYO"
+// primero
+console.log(frase.charCodeAt(0))
+// ultimo
+console.log(frase.charCodeAt(frase.length - 1))
+// por defecto
+console.log(frase.charCodeAt())
+// fuera de rango
+console.log(frase.charCodeAt(19)) //NaN
