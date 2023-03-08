f0d93119a1a33496ac9bb16e7d92f2713ae5b324
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-08 09:16:48 -0500

    feat: :rocket: Method matchAll
    The matchAll() method returns an iterator of all results matching a string against a regular expression, including capturing groups

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-//                                      match 
-/* The match() method retrieves the result of matching a string against a regular expression.*/
+//                                      matchAll 
+/* The matchAll() method returns an iterator of all results matching a string against a regular expression, including capturing groups.*/
 
-const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
-const regex = /[A-Z]/g;
-const found = paragraph.match(regex);
+const regexp = /t(e)(st(\d?))/g;
+const str = 'test1test2';
 
-console.log(found);
-// Expected output: Array ["T", "I"]
+const array = [...str.matchAll(regexp)];
+
+console.log(array[0]);
+// Expected output: Array ["test1", "e", "st1", "1"]
+
+console.log(array[1]);
+// Expected output: Array ["test2", "e", "st2", "2"]
