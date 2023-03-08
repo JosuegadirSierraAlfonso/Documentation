7e5e412dfe2372b7113ce9546c95849167475fd4
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-08 07:35:59 -0500

    feat: :tada: Method charAt
    Los caracteres de una cadena se indexan de izquierda a derecha.  El índice del primer caracter es 0, y el índice del último  caracter seria -1. Si el indice que se proporciona está fuera del rango,  JavaScript devuelve una cadena vacía

---
+++ main.js
@@ -0,0 +1,10 @@
+//                         charAt
+// Los caracteres de una cadena se indexan de izquierda a derecha. 
+// El índice del primer caracter es 0, y el índice del último 
+// caracter seria -1. Si el indice que se proporciona está fuera del rango, 
+// JavaScript devuelve una cadena vacía.
+
+const nombre = "Gadir"
+
+console.log(nombre.charAt(0))
+console.log(nombre.charAt(nombre.length - 1))
