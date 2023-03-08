679a83fcbc0807b7b9e3f0271839ddb37a24ce49
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-08 09:13:20 -0500

    feat: :rocket: Method match
    The match() method retrieves the result of matching a string against a regular expression

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-//                                      localeCompare 
-/* The localeCompare() method returns a number indicating whether 
-a reference string comes before, or after, or is the same as the 
-given string in sort order. In implementations with Intl.Collator 
-API support, this method simply calls Intl.Collator.*/
+//                                      match 
+/* The match() method retrieves the result of matching a string against a regular expression.*/
 
-const a = 'réservé'; // With accents, lowercase
-const b = 'RESERVE'; // No accents, uppercase
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
 
-console.log(a.localeCompare(b));
-// Expected output: 1
-console.log(a.localeCompare(b, 'en', { sensitivity: 'base' }));
-// Expected output: 0
+console.log(found);
+// Expected output: Array ["T", "I"]
