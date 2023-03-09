a3dae471983d2e6e5280de866f01095d094ea957
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 07:39:59 -0500

    feat: :rocket: Method Math.max
    The Math.max() static method returns the largest of the numbers given as input parameters, or -Infinity if there are no parameters

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-//                                      Math.log
-/* The Math.log() static method returns the natural logarithm (base e) of a number. That is
-∀ x > 0 , 𝙼𝚊𝚝𝚑.𝚕𝚘𝚐 ( 𝚡 ) = ln ( x ) = the unique  y  such that  e y = x */
+//                                      Math.max
+/* The Math.max() static method returns the largest of the numbers given as input parameters, or -Infinity if there are no parameters.*/
 
-function getBaseLog(x, y) {
-    return Math.log(y) / Math.log(x);
-  }
-  
-  // 2 x 2 x 2 = 8
-  console.log(getBaseLog(2, 8));
-  // Expected output: 3
-  
-  // 5 x 5 x 5 x 5 = 625
-  console.log(getBaseLog(5, 625));
-  // Expected output: 4
+console.log(Math.max(1, 3, 2));
+// Expected output: 3
+
+console.log(Math.max(-1, -3, -2));
+// Expected output: -1
+
+const array1 = [1, 3, 2];
+
+console.log(Math.max(...array1));
+// Expected output: 3
