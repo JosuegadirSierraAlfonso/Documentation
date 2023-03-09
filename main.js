47234f95001125798a032e245e7c141681ff811f
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 07:21:57 -0500

    feat: :rocket: Method Math.abs
    The Math.abs() static method returns the absolute value of a number

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-//                                      String.raw 
-/* String.raw() is the only tag function built into string templates; 
-works the same as the default template function and performs the concatenation. You can even reimplement it with normal JavaScript code.*/
+//                                      Math.abs
+/* The Math.abs() static method returns the absolute value of a number.*/
 
-console.log(`Hi\n${2+3}!`)
-
-
-console.log(String.raw`Hi\n${2+3}!`);
-// 'Hi\n5!', the character after 'Hi'
-// is not a newline character,
-// '\' and 'n' are two characters.
+function difference(a, b) {
+    return Math.abs(a - b);
+  }
+  
+  console.log(difference(3, 5));
+  // Expected output: 2
+  
+  console.log(difference(5, 3));
+  // Expected output: 2
+  
+  console.log(difference(1.23456, 7.89012));
+  // Expected output: 6.6555599999999995
