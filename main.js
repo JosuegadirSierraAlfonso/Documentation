d8d23c31e51ba2ee2b996c20de8a057e377af3e8
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:34:38 -0500

    feat: :rocket: Method Math.tan
    The Math.tan() static method returns the tangent of a number in radians

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-//                                      Math.sin
-/* The Math.sin() static method returns the sine of a number in radians.*/
+//                                      Math.tan
+/* The Math.tan() static method returns the tangent of a number in radians.*/
 
 
-function getCircleY(radians, radius) {
-    return Math.sin(radians) * radius;
+function getTanFromDegrees(degrees) {
+    return Math.tan(degrees * Math.PI / 180);
   }
   
-  console.log(getCircleY(1, 10));
-  // Expected output: 8.414709848078965
+  console.log(getTanFromDegrees(0));
+  // Expected output: 0
   
-  console.log(getCircleY(2, 10));
-  // Expected output: 9.092974268256818
+  console.log(getTanFromDegrees(45));
+  // Expected output: 0.9999999999999999
   
-  console.log(getCircleY(Math.PI, 10));
-  // Expected output: 1.2246467991473533e-15
+  console.log(getTanFromDegrees(90));
+  // Expected output: 16331239353195370
   
