59ab3a428bcafbcb496d9066313c49bfcc736985
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 06:38:48 -0500

    feat: :rocket: Method toUpperCase
    The toUpperCase() method returns the calling string value converted to uppercase (the value will be converted to a string if it isn't one)

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-//                                      toLowerCase 
-/* The toLowerCase() method returns the calling string value converted to lower case.  */
+//                                      toUpperCase 
+/* The toUpperCase() method returns the calling string value converted to uppercase (the value will be converted to a string if it isn't one).   */
 
 
-const sentence = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.';
+const sentence = 'The quick brown fox jumps over the lazy dog.';
 
-console.log(sentence.toLowerCase());
-// Expected output: "the quick brown fox jumps over the lazy dog."
+console.log(sentence.toUpperCase());
+// Expected output: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
