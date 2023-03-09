ba68954df04dabaac52ec645796d6226d863ada9
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:17:37 -0500

    feat: :rocket: Method Math.random
    The Math.random() static method returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over that range — which you can then scale to your desired range

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-//                                      Math.pow
-/* The Math.pow() static method returns the value of a base raised to a power. That is
-𝙼𝚊𝚝𝚑.𝚙𝚘𝚠 ( 𝚡 , 𝚢 ) = x y */
+//                                      Math.random
+/* The Math.random() static method returns a floating-point, pseudo-random number 
+that's greater than or equal to 0 and less than 1, with approximately uniform 
+distribution over that range — which you can then scale to your desired range. 
+The implementation selects the initial seed to the random number generation algorithm; 
+it cannot be chosen or reset by the user. */
 
-console.log(Math.pow(7, 3));
-// Expected output: 343
-
-console.log(Math.pow(4, 0.5));
-// Expected output: 2
-
-console.log(Math.pow(7, -2));
-// Expected output: 0.02040816326530612
-//                  (1/49)
-
-console.log(Math.pow(-7, 0.5));
-// Expected output: NaN
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(3));
+  // Expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // Expected output: 0
+  
+  console.log(Math.random());
+  // Expected output: a number from 0 to <1
