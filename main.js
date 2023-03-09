f04d4e65d5c8f6c886a5e3c31b5bf68e5149ed2f
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:25:29 -0500

    feat: :rocket: Method Math.floor
    The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-//                                      Math.ceil
-/* The Math.ceil() static method always rounds up and returns the smaller integer greater than or equal to a given number.*/
+//                                      Math.floor
+/* The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.*/
 
-console.log(Math.ceil(.95));
-// Expected output: 1
 
-console.log(Math.ceil(4));
-// Expected output: 4
+console.log(Math.floor(5.95));
+// Expected output: 5
 
-console.log(Math.ceil(7.004));
-// Expected output: 8
+console.log(Math.floor(5.05));
+// Expected output: 5
 
-console.log(Math.ceil(-7.004));
-// Expected output: -7
+console.log(Math.floor(5));
+// Expected output: 5
+
+console.log(Math.floor(-5.05));
+// Expected output: -6
