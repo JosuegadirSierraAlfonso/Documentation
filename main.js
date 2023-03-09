f9e635d1cea7b7dcb973a5b05358c6a5d94502f0
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:30:24 -0500

    feat: :rocket: Method Math.asin
    The Math.asin() static method returns the inverse sine (in radians) of a number

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-//                                      Math.acos
-/* The Math.acos() static method returns the inverse cosine (in radians) of a number. That is,
-∀ x ∊ [ − 1 , 1 ] , 𝙼𝚊𝚝𝚑.𝚊𝚌𝚘𝚜 ( 𝚡 ) = arccos ( x ) = the unique  y ∊ [ 0 , π ]  such that  cos ( y ) = x */
+//                                      Math.asin
+/* The Math.asin() static method returns the inverse sine (in radians) of a number. That is,
+∀ x ∊ [ − 1 , 1 ] , 𝙼𝚊𝚝𝚑.𝚊𝚜𝚒𝚗 ( 𝚡 ) = arcsin ( x ) = the unique  y ∊ [ − π 2 , π 2 ]  such that  sin ( y ) = x */
 
 
 // Calculates angle of a right-angle triangle in radians
-function calcAngle(adjacent, hypotenuse) {
-    return Math.acos(adjacent / hypotenuse);
+function calcAngle(opposite, hypotenuse) {
+    return Math.asin(opposite / hypotenuse);
   }
   
-  console.log(calcAngle(8, 10));
-  // Expected output: 0.6435011087932843
+  console.log(calcAngle(6, 10));
+  // Expected output: 0.6435011087932844
   
   console.log(calcAngle(5, 3));
   // Expected output: NaN
