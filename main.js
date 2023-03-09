b4946088472d9663ba37fc39e9092d1037f40624
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:28:15 -0500

    feat: :rocket: Method Math.acos
    The Math.acos() static method returns the inverse cosine (in radians) of a number

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-//                                      Math.round
-/* The Math.round() static method returns the value of a number rounded to the nearest integer.*/
+//                                      Math.acos
+/* The Math.acos() static method returns the inverse cosine (in radians) of a number. That is,
+∀ x ∊ [ − 1 , 1 ] , 𝙼𝚊𝚝𝚑.𝚊𝚌𝚘𝚜 ( 𝚡 ) = arccos ( x ) = the unique  y ∊ [ 0 , π ]  such that  cos ( y ) = x */
 
 
-console.log(Math.round(0.9));
-// Expected output: 1
-
-console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
-// Expected output: 6 6 5
-
-console.log(Math.round(-5.05), Math.round(-5.5), Math.round(-5.95));
-// Expected output: -5 -5 -6
+// Calculates angle of a right-angle triangle in radians
+function calcAngle(adjacent, hypotenuse) {
+    return Math.acos(adjacent / hypotenuse);
+  }
+  
+  console.log(calcAngle(8, 10));
+  // Expected output: 0.6435011087932843
+  
+  console.log(calcAngle(5, 3));
+  // Expected output: NaN
