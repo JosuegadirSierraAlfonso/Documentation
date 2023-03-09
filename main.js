6e561922f56b05b7fc00bfab18cd2cdc69b37f62
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 08:31:35 -0500

    feat: :rocket: Method Math.atan
    The Math.atan() static method returns the inverse tangent (in radians) of a number

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-//                                      Math.asin
-/* The Math.asin() static method returns the inverse sine (in radians) of a number. That is,
-∀ x ∊ [ − 1 , 1 ] , 𝙼𝚊𝚝𝚑.𝚊𝚜𝚒𝚗 ( 𝚡 ) = arcsin ( x ) = the unique  y ∊ [ − π 2 , π 2 ]  such that  sin ( y ) = x */
+//                                      Math.atan
+/* The Math.atan() static method returns the inverse tangent (in radians) of a number, that is
+𝙼𝚊𝚝𝚑.𝚊𝚝𝚊𝚗 ( 𝚡 ) = arctan ( x ) = the unique  y ∊ [ − π 2 , π 2 ]  such that  tan ( y ) = x */
 
 
 // Calculates angle of a right-angle triangle in radians
-function calcAngle(opposite, hypotenuse) {
-    return Math.asin(opposite / hypotenuse);
+function calcAngle(opposite, adjacent) {
+    return Math.atan(opposite / adjacent);
   }
   
-  console.log(calcAngle(6, 10));
-  // Expected output: 0.6435011087932844
+  console.log(calcAngle(8, 10));
+  // Expected output: 0.6747409422235527
   
   console.log(calcAngle(5, 3));
-  // Expected output: NaN
+  // Expected output: 1.0303768265243125
