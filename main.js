33e34d1d84146be0ff67dce9064c57197c9801c8
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-09 07:28:51 -0500

    feat: :rocket: Method Math.exp
    The Math.exp() static method returns e raised to the power of a number

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-//                                      Math.abs
-/* The Math.abs() static method returns the absolute value of a number.*/
+//                                      Math.exp
+/* The Math.exp() static method returns e raised to the power of a number. That is    𝙼𝚊𝚝𝚑.𝚎𝚡𝚙 ( 𝚡 ) = e x 
+Because exp() is a static method of Math, you always use it as Math.exp(), rather than as a method of a Math object you created (Math is not a constructor).*/
 
-function difference(a, b) {
-    return Math.abs(a - b);
-  }
-  
-  console.log(difference(3, 5));
-  // Expected output: 2
-  
-  console.log(difference(5, 3));
-  // Expected output: 2
-  
-  console.log(difference(1.23456, 7.89012));
-  // Expected output: 6.6555599999999995
+console.log(Math.exp(0));
+// Expected output: 1
+
+console.log(Math.exp(1));
+// Expected output: 2.718281828459 (approximately)
+
+console.log(Math.exp(-1));
+// Expected output: 0.36787944117144233
+
+console.log(Math.exp(2));
+// Expected output: 7.38905609893065
