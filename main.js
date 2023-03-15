04c40b63da9e662f1db2ca515c69dc5f7975031f
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-15 06:13:46 -0500

    feat: :rocket: functions by declaration
    The function declaration defines a function with the specified parameters. You can also define functions using the Function constructor and a function expression

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
-//                           FormData      
-/* The FormData object lets you compile a set of key/value 
-pairs to send using XMLHttpRequest. It is primarily intended 
-for use in sending form data, but can be used independently 
-from forms in order to transmit keyed data. The transmitted 
-data is in the same format that the form's submit() method 
-would use to send the data if the form's encoding type were 
-set to multipart/form-data.*/
+//                           functions by declaration
+/* The function declaration defines a function with the specified parameters.
+You can also define functions using the Function constructor and a function expression.*/
 
 
-const formData = new FormData();
+function calcRectArea(width, height) {
+    return width * height;
+}
 
-formData.append("username", "Groucho");
-formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
 
-// HTML file input, chosen by user
-formData.append("userfile", fileInputElement.files[0]);
-
-// JavaScript file-like object
-const content = '<q id="a"><span id="b">hey!</span></q>'; // the body of the new file…
-const blob = new Blob([content], { type: "text/xml" });
-
-formData.append("webmasterfile", blob);
-
-const request = new XMLHttpRequest();
-request.open("POST", "http://foo.com/submitform.php");
-request.send(formData);
+console.log(calcRectArea(5, 6));
+// Expected output: 30
