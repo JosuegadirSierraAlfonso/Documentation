5a9d6c7c0adc9bebf8eeb957eacf433335afae2c
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-03-15 06:42:06 -0500

    feat: :rocket: Lexical scope
    Lexical scope describes how nested functions (also known as "children") have access to variables defined in the boundaries of their parents

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-//                  Arrow functions
-/* An arrow function expression is a compact alternative to a traditional function expression,
-with some semantic differences and deliberate limitations in usage:
+//                  Lexical scope
+/*Lexical scope describes how nested functions (also known as "children")
+have access to variables defined in the boundaries of their parents.*/
 
 
-Arrow functions don't have their own bindings to this,
-arguments, or super, and should not be used as methods.
-Arrow functions cannot be used as constructors.
-Calling them with new throws a TypeError.
- They also don't have access to the new.target keyword.
-Arrow functions cannot use yield within their body and cannot be created as generator*/
+const miFuncion = () => {
+    let miValor = 2;
+    console.log(miValor);
 
+    const funcionHija = () => {
+         console.log(miValor += 1);
+    }
 
-const materials = [
-    'Hydrogen',
-    'Helium',
-    'Lithium',
-    'Beryllium'
-  ]
- 
-  console.log(materials.map(material => material.length))
-  // Expected output: Array [8, 6, 7, 9]
+    funcionHija()
+}
+
+miFuncion()
