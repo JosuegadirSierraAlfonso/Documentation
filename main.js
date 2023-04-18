047c5ce2b5a0508e60a6d8357aae0d3bc2bf1636
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-04-18 09:26:52 -0500

    feat: :memo: Promises
    Promises in JavaScript are a mechanism to handle asynchronous operations in an easier and more readable way.

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-//                  Callback
-/*A callback in JavaScript is a function that is passed as 
-an argument to another function and is called inside the main function at some specific time.*/
+//                  Promises
+/*
+Una Promesa es un proxy de un valor que no necesariamente se conoce cuando se crea la promesa. 
+Le permite asociar controladores con el valor eventual de éxito o el motivo de falla de una 
+acción asíncrona. Esto permite que los métodos asíncronos devuelvan valores como los métodos 
+síncronos: en lugar de devolver inmediatamente el valor final, el método asíncrono devuelve 
+la promesa de proporcionar el valor en algún momento en el futuro.*/
 
 
-function multiplicar(num1, num2, callback) {
-    let resultado = num1 * num2;
-    callback(resultado);
+function multiplicar(num1, num2) {
+    return new Promise(function(resolve, reject) {
+      let resultado = num1 * num2;
+      if(resultado >= 0) {
+        resolve(resultado);
+      } else {
+        reject("El resultado es negativo");
+      }
+    });
   }
   
-  function mostrarResultado(res) {
-    console.log("El resultado es: " + res);
-  }
+  multiplicar(6, 10)
+    .then(function(resultado) {
+      console.log("El resultado es: " + resultado);
+    })
+    .catch(function(error) {
+      console.log("Error: " + error);
+    });
   
-  multiplicar(5, 10, mostrarResultado); // Muestra "El resultado es: 50"
